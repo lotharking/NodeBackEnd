fd7be351a2a16b4cfee7473051be54bb0dfa1cc1
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-03-23 20:47:19 -0500

    validation

--- prueba/src/libros/dto/crear-libros.dto.ts
+++ prueba/src/libros/dto/crear-libros.dto.ts
@@ -1,5 +1,19 @@
+import { IsString, IsNotEmpty, IsNumber, IsPositive, Max, Min } from "class-validator";
+
 export class CrearLibroDto{
+
+    @IsString()
+    @IsNotEmpty()
     nombre: String;
+
+    @IsString()
+    @IsNotEmpty()
     autor: String;
+
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
+    @Max(1200)
+    @IsNotEmpty()
     pags: number;
 }
