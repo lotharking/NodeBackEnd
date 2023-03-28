7ab0a98b5e974fed5969a3f6f4938776cea6335f
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-03-27 20:01:13 -0500

    async response

--- TypeScript-intro/src/bases/03-classes.ts
+++ TypeScript-intro/src/bases/03-classes.ts
@@ -1,8 +1,20 @@
+import axios from 'axios';
+
 export class Pokemon {
 
     constructor(
         public id: number, 
         public name: string) {}
+    
+    /**Method Async for promise */
+    async getmoves(){
+        //await -> wait the response from the api
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        console.log(data.moves);
+
+        return data.moves;
+    }
 }
 
 export const charmander = new Pokemon(4, 'Charmander');
+
