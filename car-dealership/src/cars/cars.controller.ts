c4ef8ed2802fe0d6a3fa97ecda520cd32476f4c6
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-04-04 01:01:19 -0500

    get id

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota', 'Honda', 'Tercero']
+
     @Get()
     getAllCars() {
-        return ['Toyota', 'Honda', 'Tercero']
+        return this.cars
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ){
+        return this.cars[id];
     }
 
 }
