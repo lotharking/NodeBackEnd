632bb51d6032f48b355a78a937b3b31065a1b86b
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-04-25 00:17:54 -0500

    brands created

--- car-dealership/src/seed/seed.service.ts
+++ car-dealership/src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from 'src/brands/brands.service';
+import { BRANDS_SEED } from './data/brand.seed';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
   populeteDB() {
+    this.carsService.firstCarsWithSeedDate( CARS_SEED );
+    this.brandsService.firstCarsWithSeedDate( BRANDS_SEED );
     return 'Seed executed'
   }
 }
