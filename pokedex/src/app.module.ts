6cbec0dcddae84eeef31cde05e93a2b6523cd1e9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-04-26 00:30:59 -0500

    add format with prettier

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
-    PokemonModule
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
