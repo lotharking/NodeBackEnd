71a8c88bc5f8fc9ebd6b1cf170404f5b916db646
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-04-26 00:27:11 -0500

    add mongoose

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule
+  ],
   controllers: [],
   providers: [],
 })
