289922b7dc112dc3f7e68f98be487111d078d458
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-05-17 00:23:49 -0500

    Feat: create seed

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
