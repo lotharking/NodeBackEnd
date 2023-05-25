0c4bd688ff09245a214d52d589593a30230c2190
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-05-24 20:43:11 -0500

    Feat: seed fix to ten pokemon

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interfaces';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSEED() {
-    const { data } = await this.axios.get('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/')
+      const no = +segments[ segments.length - 2 ]
+
+      const pokemon = await this.pokemonModel.create({ name, no }) 
+      console.log({ name, no })
+    })
     return data.results;
   }
 }
