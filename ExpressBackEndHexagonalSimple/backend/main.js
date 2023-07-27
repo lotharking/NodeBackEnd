dcd6a44e282cd37ba89ad5da80dc61472b56de5f
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-07-27 00:34:37 -0500

    Feat: add CORS

--- ExpressBackEndHexagonalSimple/backend/main.js
+++ ExpressBackEndHexagonalSimple/backend/main.js
@@ -7,6 +7,14 @@ const port = 3000;
 
 app.use(express.json());
 
+// Permite las peticiones del frontend 'http://127.0.0.1:5500'
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/movies', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
